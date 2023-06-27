bc14406f530ffe9e8e80147de169ea8a669bcb5b
SergeiGrub <sergeygrubber@gmail.com>
2023-06-27 16:40:56 +0300

    1) Вычислить n-ое треугольного число

--- src/main/java/Homework/Task_1.java
+++ src/main/java/Homework/Task_1.java
@@ -1,10 +1,12 @@
-package Homework;
-import java.util.Scanner;
-
 /*
 Урок 1. Знакомство с языком программирования Java
 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 */
+
+package Homework;
+import java.util.Scanner;
+
+
 public class Task_1 {
     static int fctrl(int num) {
         if (num == 1) return 1;
