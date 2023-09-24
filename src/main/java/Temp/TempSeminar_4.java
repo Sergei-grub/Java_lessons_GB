c4c39bcc6f257453b1b6035b335f5f22c7b0b224
Sergei <sergeygrubber@gmail.com>
2023-09-24 23:46:51 +0300

    + Seminar 6

--- src/main/java/Temp/TempSeminar_4.java
+++ src/main/java/Temp/TempSeminar_4.java
@@ -1,6 +1,5 @@
 package Temp;
 
-import java.sql.Array;
 import java.util.*;
 
 public class TempSeminar_4 {
@@ -34,41 +33,6 @@ public class TempSeminar_4 {
             }
         }
 
-
-        Set<Integer> HS = new HashSet<>(List.of(2, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        Set<Integer> lktHS = new LinkedHashSet<>(List.of(2, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        Set<Integer> trHS = new TreeSet<>(List.of(2, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-
-//        System.out.println(arr.getClass().getSimpleName());
-//        System.out.println(s.getClass().getSimpleName());
-//        System.out.println(arr.getClass());
-//        System.out.println(s.getClass());
-//        System.out.println(isempty(arr));
-//        System.out.println();
-//        System.out.println(HS);
-//        System.out.println(lktHS);
-//        System.out.println(trHS);
-//        System.out.println();
-//        System.out.println(HS.getClass());
-//        System.out.println(lktHS.getClass());
-//        System.out.println(trHS.getClass());
-//        System.out.println();
-//        System.out.println(HS.getClass().getSimpleName());
-//        System.out.println(lktHS.getClass().getSimpleName());
-//        System.out.println(trHS.getClass().getSimpleName());
-
-
-
-        System.out.println("Кол-во уникальных значений: " + sortSet(arr) + "%");
-
     }
 
-    public static float sortSet(int[] arr) {
-        Set<Integer> arHS = new HashSet<>();
-        for (int j : arr) {
-            arHS.add(j);
-            System.out.println(j);
-        }
-        return (float) (arHS.size() * 100 / arr.length);
-    }
 }
