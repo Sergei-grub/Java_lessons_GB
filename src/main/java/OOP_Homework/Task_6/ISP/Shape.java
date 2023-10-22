72160f51851c9b8e4a30358b5ff845aba22520aa
Sergei <sergeygrubber@gmail.com>
2023-10-22 19:24:56 +0300

    Task 6

--- src/main/java/OOP_Homework/Task_6/ISP/Shape.java
+++ src/main/java/OOP_Homework/Task_6/ISP/Shape.java
@@ -2,7 +2,7 @@ package OOP_Homework.Task_6.ISP;
 
 // Принципы SOLID
 // I — Interface Segregation (Принцип разделения интерфейсов)
-// Для класса "Cube" был создан отдельный интерфейс "Shape3D".
+// Интерфейсы были разделены. Для класса "Cube" был создан отдельный интерфейс "Shape3D".
 public interface Shape {
     public double area();
 
